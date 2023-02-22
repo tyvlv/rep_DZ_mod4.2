e3b5766b2c6aa8b812e33286b880077e67a5cb36
tychkov.vv <tyvl@yandex.ru>
2023-02-22 18:51:07 +0300

    Do hometask #2

--- main.py
+++ main.py
@@ -2,11 +2,26 @@ from classes import Channel
 
 
 def main():
-    shulman_channel = Channel('UCL1rJ0ROIw9V1qFeIN0ZTZQ')  # Екатерина Шульман
-    vdud_channel = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')  # вДудь
+    shulman = Channel('UCL1rJ0ROIw9V1qFeIN0ZTZQ')  # Екатерина Шульман
+    vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')  # вДудь
 
-    shulman_channel.print_info()
-    vdud_channel.print_info()
+    shulman.print_info()
+    vdud.print_info()
+
+    print(vdud.title)
+    print(vdud.description)
+    print(vdud.url)
+    print(vdud.subscriber_count)
+    print(vdud.video_count)
+    print(vdud.view_count)
+
+    print(vdud.channel_id)
+    # vdud.channel_id = "Новое название"
+
+    print(Channel.get_service())
+
+    vdud.to_json('vdud.json')
+    shulman.to_json("shulman.json")
 
 
 if __name__ == "__main__":
